7c2d251a9ebee48911e350795ab031c49798b705
ChillYall <mariellehoneypchiong@gmail.com>
2023-05-16 11:57:55 +0800

    compared week 3 with instructor's sample code

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,5 +1,37 @@
-import { getParam } from "./utils.mjs";
-import productDetails from "./productDetails.mjs";
+// Team's code from lines 2-6
+// import { getParam } from "./utils.mjs";
+// import productDetails from "./productDetails.mjs";
 
-const productId = getParam("product");
-productDetails(productId);
+// const productId = getParam("product");
+// productDetails(productId);
+
+// Needs to be reviewed (code from instructor's sample)
+import { findProductById } from "./productData.mjs";
+import { setLocalStorage } from "./utils.mjs";
+
+let product = {};
+
+export default async function productDetails(productId) {
+  // get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  renderProductDetails();
+  // once the HTML is rendered we can add a listener to Add to Cart button
+  document.getElementById("addToCart").addEventListener("click", addToCart);
+}
+function addToCart() {
+  setLocalStorage("so-cart", product);
+}
+function renderProductDetails() {
+  document.querySelector("#productName").innerText = product.Brand.Name;
+  document.querySelector("#productNameWithoutBrand").innerText =
+    product.NameWithoutBrand;
+  document.querySelector("#productImage").src = product.Image;
+  document.querySelector("#productImage").alt = product.Name;
+  document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
+  document.querySelector("#productColorName").innerText =
+    product.Colors[0].ColorName;
+  document.querySelector("#productDescriptionHtmlSimple").innerHTML =
+    product.DescriptionHtmlSimple;
+  document.querySelector("#addToCart").dataset.id = product.Id;
+}
