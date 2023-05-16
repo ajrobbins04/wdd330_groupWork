b3558066cf33019d493b7355d1988e24b0d31a2d
ajrobbins04 <ajrobbins04@gmail.com>
2023-05-15 19:41:09 -0700

    fixing up week 03 activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,10 @@
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
  
-  
+function run() {
 const productId = getParam("product");
 
 productDetails(productId);
+}
 
   
