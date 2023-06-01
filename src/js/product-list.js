5d7375365df469469e4368cbf2017736a82dd0fc
ChillYall <mariellehoneypchiong@gmail.com>
2023-06-01 21:47:07 +0800

    week 6

--- src/js/product-list.js
+++ src/js/product-list.js
@@ -1,8 +1,7 @@
 import productList from "../js/productList.mjs";
 import { getParam, loadHeaderFooter } from "../js/utils.mjs";
-import { getData } from "./productData.mjs";
 
-getParam();
-getData();
+
 loadHeaderFooter();
-productList(".product-list", "tents");
+const category = getParam("category");
+productList(".product-list", category);
